bc7988882765e099a689c1ff8ff87942c23dacb9
ma2snct <knightmarespark@gmail.com>
2015-05-05 11:05:11 +0900

    try to use cleardb ,again

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var app = express();
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-	user: 'b0ea7ca3e0c1ec',
+	/* ローカル用
+	user: 'todo',//'b0ea7ca3e0c1ec',
+	password: 'todo',//'62a7bbb5',
+	database: 'myappdb'//'heroku_3cf5429302fd605'
+	*/
+	user:'b0ea7ca3e0c1ec',
 	password: '62a7bbb5',
-	database: 'heroku_3cf5429302fd605'
+	database: 'heroku_3cf5429302fd605',
+	host: 'us-cdbr-iron-east-02.cleardb.net'
 });
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,10 +19,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
 	//response.send('This is my todo list')
-	var rank = 70;
-	var todo = 'date with my girlfriend';
+	var rank = 90;
+	var todo = 'go to workspace';
 	connection.query('INSERT INTO todolist SET rank = ?, todo = ?', [rank, todo]);
-	response.send('add todo to database table')
+	response.send('add todo to database table on heroku')
 });
 
 app.listen(app.get('port'), function(){
