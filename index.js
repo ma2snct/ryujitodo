780a5b9084f25cf779f3817ed40d263b23b67418
ma2snct <knightmarespark@gmail.com>
2015-05-07 01:09:22 +0900

    try to show table

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ var connection = mysql.createConnection({
 	password: 'todo',//'62a7bbb5',
 	database: 'myappdb'//'heroku_3cf5429302fd605'
 	*/
+	
+	/* デプロイ用*/
 	user:'b0ea7ca3e0c1ec',
 	password: '62a7bbb5',
 	database: 'heroku_3cf5429302fd605',
 	host: 'us-cdbr-iron-east-02.cleardb.net'
+	
 });
 
 app.set('port', (process.env.PORT || 5000));
@@ -31,25 +34,31 @@ app.get('/', function(request, response){
 
 
 app.get('/todo', function(request, response){
+	/*
 	connection.connect();
 	var query = connection.query('SELECT * FROM todolist;');
 	query
 		.on('results', function(rows){
 			response.send(rows);
+			console.log(rows);
 		})
 
 		.on('error', function(err){
 			response.send(err);
+			console.log(err);
 		})
 
 
-	
+	*/
 	//response.send('this is todo list page.')
-	/*var query = client.query('SELECT * FROM todolist;');
-	query.on('row', function(row){
+	var query = connection.query('SELECT * FROM todolist;', function(err, results, fields){
+
+	//query.on('row', function(row){
 		console.log('get row:');
-		console.log(row);
-	});*/
+		console.log(results);
+		response.send('the most important todo is ' + results[1].todo);
+		//response.send(results[0].todo);
+	})
 });
 
 
