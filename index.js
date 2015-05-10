638102adae9dcf3f831a1ad62af95bddd170b0c0
ma2snct <knightmarespark@gmail.com>
2015-05-10 23:23:21 +0900

    jadeの文法試してます

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/todo', function(request, response){
 app.get('/alltodo', function(request, response){
 	//全件表示したいけど、できてない
 	connection.query('SELECT * FROM todolist;', function(err, results, fields){
-		response.render('todo.jade', {todos:results}});
+		response.render('todo.jade', {todos:results});
 	});
 
 
